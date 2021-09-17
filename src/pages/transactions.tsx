9f7ba81397f716efaba2d0a827e666835968a3b9
Elves Brito <elvesbd@yahoo.com>
2021-09-17 18:34:52 -0300

    feat: added docker and keycloak authenticated

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,7 +1,21 @@
-import { Column } from '@devexpress/dx-react-grid';
+import {
+  Column,
+  IntegratedFiltering,
+  IntegratedPaging,
+  PagingState,
+  SearchState,
+  SortingState,
+} from '@devexpress/dx-react-grid';
 import { Container, Typography } from "@material-ui/core";
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
-import { SortingState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  Toolbar,
+  SearchPanel,
+  PagingPanel,
+} from '@devexpress/dx-react-grid-material-ui';
+
 const columns: Column[] = [
   {
     name: 'payment_day',
@@ -25,7 +39,7 @@ const columns: Column[] = [
   }
 ];
 
-const TransactionsPage = (props) => {
+const TransactionsPage = () => {
   return (
     <Container>
       <Typography component="h1" variant="h4">
@@ -34,8 +48,15 @@ const TransactionsPage = (props) => {
 
       <Grid rows={[]} columns={columns}>
         <Table />
+        <SearchState defaultValue="Paris"/>
         <SortingState defaultSorting={[{ columnName: 'created_at', direction: 'asc'}]}/>
+        <PagingState defaultCurrentPage={0} pageSize={5}/>
         <TableHeaderRow showSortingControls/>
+        <IntegratedFiltering />
+        <Toolbar />
+        <SearchPanel />
+        <PagingPanel />
+        <IntegratedPaging />
       </Grid>
     </Container>
   );
